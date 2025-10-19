b75435be767dd0dd64c4ab1afd33407ba5cc0001
Thales <thales0591@gmail.com>
2025-10-19 18:58:39 -0300

    feat: integrate with .env

--- src/server.ts
+++ src/server.ts
@@ -6,9 +6,12 @@ app.get("/", async (request, reply) => {
   return reply.send().status(200);
 });
 
+const PORT = Number(process.env.PORT) || 3000;
+const HOST = process.env.HOST || "0.0.0.0";
+
 app
   .listen({
-    host: "0.0.0.0",
-    port: 3333,
+    host: HOST,
+    port: PORT,
   })
-  .then(() => [console.log("🚀 HTTP Server Running at 3333!")]);
+  .then(() => [console.log(`🚀 HTTP Server Running on ${HOST}:${PORT}!`)]);
