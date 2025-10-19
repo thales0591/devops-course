79889828a3183a32ad6b7d379cf8f94158f72e04
Thales <thales0591@gmail.com>
2025-10-19 10:50:29 -0300

    feat: create fastify server

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import { fastify } from "fastify";
+
+export const app = fastify();
+
+app.get("/", async (request, reply) => {
+  return reply.send().status(200);
+});
+
+app
+  .listen({
+    host: "0.0.0.0",
+    port: 3333,
+  })
+  .then(() => [console.log("🚀 HTTP Server Running!")]);
