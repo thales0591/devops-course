792f6956f3ef4ca9b8ea310f5155fe597b4c3ec9
Thales <thales0591@gmail.com>
2025-10-26 16:23:43 -0300

    feat: add terraform workspace name into bucket name

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "s3_bucket" {
-  bucket = "rocketseat-bucket-iac-thales0591"
+  bucket = "rocketseat-bucket-iac-thales0591-${terraform.workspace}"
 
   tags = {
     Name = "primeiro bucket"
