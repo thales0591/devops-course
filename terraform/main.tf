091e57cd4fef35fca4b598cc709d52aead5d4b84
Thales <thales0591@gmail.com>
2025-10-27 20:39:56 -0300

    feat: add sqs module from terraform modules website

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,3 +18,12 @@ module "cloudfront" {
     module.s3
   ]
 }
+
+module "sqs" {
+  source  = "terraform-aws-modules/sqs/aws"
+  name = "example"
+  create_dlq = true
+  tags = {
+    Iac = true
+  }
+}
