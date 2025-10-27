ef27cdc9240618eb52de7e6a36cd7bf4e7bca6b4
Thales <thales0591@gmail.com>
2025-10-27 14:58:55 -0300

    feat: create cloudfront module and configure its origin variables from s3 module outputs

---
+++ terraform/modules/cloudfront/variables.tf
@@ -0,0 +1,9 @@
+variable "origin_id" {
+  type        = string
+  description = "Id do bucket s3"
+}
+
+variable "bucket_domain_name" {
+  type        = string
+  description = "URL do bucket s3"
+}
--- terraform/modules/s3/outputs.tf
+++ terraform/modules/s3/outputs.tf
@@ -1,5 +1,11 @@
-output bucket_domain_name {
-  value       = ""
+output "bucket_domain_name" {
+  value       = aws_s3_bucket.bucket.bucket_domain_name
   sensitive   = false
-  description = "URL do bucket"
+  description = "URL do bucket s3"
+}
+
+output "bucket_id" {
+  value       = aws_s3_bucket.bucket.id
+  sensitive   = false
+  description = "Id de domínio do bucket s3"
 }
