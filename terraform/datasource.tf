dcef3c4e602352e254af46690e584d6cc925c035
Thales <thales0591@gmail.com>
2025-10-27 10:52:31 -0300

    feat: create org_name variable

--- terraform/datasource.tf
+++ terraform/datasource.tf
@@ -1,3 +1,3 @@
 data "aws_s3_bucket" "bucket" {
-  bucket = "rocketseat-bucket-iac-thales0591-${terraform.workspace}"
+  bucket = "${var.org_name}-bucket-iac-thales0591-${terraform.workspace}"
 }
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "s3_bucket" {
-  bucket = "rocketseat-bucket-iac-thales0591-${terraform.workspace}"
+  bucket = "${var.org_name}-bucket-iac-thales0591-${terraform.workspace}"
 
   tags = {
     Name = "primeiro bucket"
